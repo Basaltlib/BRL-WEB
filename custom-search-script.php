c13b3107de907b51f3dc2498946889c08bc6a3bd
Basalt Regional Library <devel@basaltlibrary.org>
2017-05-31 12:23:52 -0600

    Update to project. Numerous pages and styling added

---
+++ custom-search-script.php
@@ -0,0 +1,3 @@
+<?php
+	$_GET["keys"]
+	$_GET["search-radio"]
---
+++ pika.php
@@ -0,0 +1,21 @@
+<?php
+	//pull variable from serach form
+		 $keys = $_GET['keys']
+	?>
+
+	<html>
+		<head>
+		</head>
+	<body>
+	<h1>
+	<? echo 'The search term is ';
+	?>
+	<i>
+	<? echo $keys;
+	?>
+	</i>
+	</h1>
+	</body>
+	</html>
+
+
